a5a2a2ce9068893db7ccfc843bbd75cc7fd597f8
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2022-04-06 03:19:09 +0000

    Achieved Inheritance Enlightenment

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,20 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+  
+class DiceSet
+  attr_reader :name
+
+  def roll(sides)
+    @values = Array.new(sides) { rand(1..6) }
+  end
+
+  def values
+    return @values
+  end
+
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
